d111dd32933a8830a5400d561c7f93ba99efe42c
草鞋没号 <308487730@qq.com>
2022-08-29 16:21:33 +0800

    feat(🌱): side menu

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,15 +1,35 @@
-import { defineComponent } from 'vue'
+import { computed, defineComponent, ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { routes } from '@/router'
+import { classname } from '@/utils'
 import './index.less'
 
 export default defineComponent({
   name: 'layout',
   setup(props, ctx) {
+    const route = useRoute()
+    const router = useRouter()
+    const menus = ref()
 
-    console.log(ctx)
+    const genMenus = () => routes.filter(r => r.meta?.show !== false).map(r => (
+      <div class={classname(['menu-item', { active: route.path === r.path }])}>
+        <div
+          title={r.meta?.title as string}
+          class='inner-box'
+          onClick={() => router.push(r.path)}
+        >
+          {r.meta?.icon}
+        </div>
+      </div>
+    ))
+
+    watch(() => route.path, () => menus.value = genMenus(), { immediate: true })
 
     return () => (
       <div class='app-layout d-flex h-100'>
-        <div class='app-side'></div>
+        <div class='app-side'>
+          {menus.value}
+        </div>
         <div class='app-main flex-fill'>
           {/* @ts-ignore */}
           {ctx.slots?.default()}
--- src/router.tsx
+++ src/router.tsx
@@ -3,25 +3,47 @@ import {
   createRouter,
   createWebHashHistory,
 } from 'vue-router'
+import {
+  HomeOutlined,
+  SettingOutlined,
+} from '@ant-design/icons-vue'
 import Home from './views/home'
 import Config from './views/config'
 import NotFound from './views/not-found'
+
+export interface MetaRecord {
+  title?: string
+  icon?: JSX.Element
+  show?: boolean
+}
 
 export const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'home',
     component: Home,
+    meta: {
+      title: '首页',
+      icon: <HomeOutlined />,
+    },
   },
   {
     path: '/config',
     name: 'config',
     component: Config,
+    meta: {
+      title: '配置',
+      icon: <SettingOutlined />,
+    },
   },
   {
     path: '/:pathMatch(.*)*',
     name: 'not-found',
     component: NotFound,
+    meta: {
+      title: '404',
+      show: false,
+    },
   },
 ]
 
