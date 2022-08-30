96204588335f44580dfd7c4420250f71d155ee79
草鞋没号 <308487730@qq.com>
2022-08-30 08:13:33 +0800

    feat(🌱): devtool

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,10 @@
-import { defineComponent, ref, watch } from 'vue'
+import { defineComponent } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
+import {
+  ArrowLeftOutlined,
+  ArrowRightOutlined,
+  ReloadOutlined,
+} from '@ant-design/icons-vue'
 import { routes } from '@/router'
 import { classname } from '@/utils'
 import './index.less'
@@ -9,21 +14,6 @@ export default defineComponent({
   setup(props, ctx) {
     const route = useRoute()
     const router = useRouter()
-    const menus = ref()
-
-    const genMenus = () => routes.filter(r => r.meta?.show !== false).map(r => (
-      <div class={classname(['menu-item', { active: route.path === r.path }])}>
-        <div
-          title={r.meta?.title as string}
-          class='inner-box'
-          onClick={() => router.push(r.path)}
-        >
-          {r.meta?.icon}
-        </div>
-      </div>
-    ))
-
-    watch(() => route.path, () => menus.value = genMenus(), { immediate: true })
 
     return () => (
       <div class='app-layout d-flex h-100'>
@@ -31,12 +21,27 @@ export default defineComponent({
           <div class='menu-item'>
             <div class='logo'>草</div>
           </div>
-          {menus.value}
+          {routes.filter(r => r.meta?.show !== false).map(r => (
+            <div class={classname(['menu-item', { active: route.path === r.path }])}>
+              <div
+                title={r.meta?.title as string}
+                class='inner-box'
+                onClick={() => router.push(r.path)}
+              >
+                {r.meta?.icon}
+              </div>
+            </div>
+          ))}
         </div>
         <div class='app-main flex-fill'>
           <div class='app-header'>
-            <div class='dev-tool'>
-
+            <div class='devtool'>
+              <div class='btn-group'>
+                <ArrowLeftOutlined onClick={() => router.back()} />
+                <ArrowRightOutlined onClick={() => router.forward()} />
+                <ReloadOutlined onClick={() => location.reload()} />
+              </div>
+              <input class='ml-2' value={route.path} />
             </div>
           </div>
           <div class='app-content'>
