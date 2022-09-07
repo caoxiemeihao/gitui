ea3101b00377b4d637d24df62a6fb42eabb358ad
草鞋没号 <308487730@qq.com>
2022-09-07 19:58:41 +0800

    feat: open devtools by default

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,8 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -10,7 +12,11 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            app.get_window("main").unwrap().open_devtools();
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet,])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
