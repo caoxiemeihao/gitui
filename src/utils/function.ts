43d0c9d82759901347f916255b313d3297ef90f9
草鞋没号 <308487730@qq.com>
2022-09-19 09:42:34 +0800

    add utils/function.ts

---
+++ src/utils/function.ts
@@ -0,0 +1,70 @@
+
+/**
+ * ```
+ * a > b => a
+ * a < b => b
+ * a = b => undefined
+ * ```
+ */
+export function compareStr(a: string, b: string) {
+  const length = Math.max(a.length, b.length)
+  for (let i = 0; i < length; i++) {
+    const aCode = a.charCodeAt(i)
+    const bCode = b.charCodeAt(i)
+    if (Number.isNaN(aCode)) { // b longer
+      return b
+    } else if (Number.isNaN(bCode)) { // a longer
+      return a
+    } else {
+      return bCode - aCode > 0 ? b : a
+    }
+  }
+}
+
+export function walk<T extends Record<string, any> = any>(
+  tree: T,
+  cb: (tree: T, ancestor: T[]) => (T | void),
+  ancestor: T[] = [],
+) {
+  ancestor = ancestor.concat(tree)
+  for (const key of Object.keys(tree)) {
+    const value = tree[key]
+    if (Array.isArray(value)) {
+      (tree as Record<string, any>)[key] = value.map(e => walk<T>(e, cb, ancestor))
+    }
+  }
+
+  if (cb) {
+    const tmp = cb(tree, ancestor)
+    if (tmp) {
+      tree = tmp
+    }
+  }
+  return tree
+}
+
+walk.async = async function walkAsync<T extends Record<string, any> = any>(
+  tree: T,
+  cb: (tree: T, ancestor: T[]) => Promise<T | void>,
+  ancestor: T[] = [],
+) {
+  ancestor = ancestor.concat(tree)
+  for (const key of Object.keys(tree)) {
+    const value = tree[key]
+    if (Array.isArray(value)) {
+      const _value: T[] = []
+      for (const val of value) {
+        _value.push(await walkAsync(val, cb, ancestor))
+      }
+      (tree as Record<string, any>)[key] = _value
+    }
+  }
+
+  if (cb) {
+    const tmp = await cb(tree, ancestor)
+    if (tmp) {
+      tree = tmp
+    }
+  }
+  return tree
+}
